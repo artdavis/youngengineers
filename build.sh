18548ab47c7d512cd4256124fab26e3894bea914
Art Davis (fractal) <art.davis@gmail.com>
2016-09-16 16:34:38 -0400

    Get rid of supplemental dir and fix adoc refs
    Fing for supplemental workshops just live in workshops/ directory. Also
    change file references to .asciidoc extensions to .adoc

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@
 # Build the pdf and html documentation
 #
 
-a2x -f pdf --icons --dblatex-opts='--verbose --style=db2latex --fig-path=/etc/asciidoc/images/icons' Young_Engineers_Club_Curriculum.asciidoc
-a2x -f xhtml --icons Young_Engineers_Club_Curriculum.asciidoc
+a2x -f pdf --icons --dblatex-opts='--verbose --style=db2latex --fig-path=/etc/asciidoc/images/icons' Young_Engineers_Club_Curriculum.adoc
+a2x -f xhtml --icons Young_Engineers_Club_Curriculum.adoc
 
