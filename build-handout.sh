42e58213394a1515f117e8ca47ba4ac94b075d87
Art Davis (fractal) <art.davis@gmail.com>
2016-10-02 18:42:58 -0400

    Minor updates

--- build-handout.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-#
-# build.sh
-# Build the pdf and html documentation
-#
-
-a2x -f xhtml --icons --dblatex-opts "-P latex.output.revhistory=0" $1
-wkhtmltopdf ${1%adoc}html ${1%adoc}pdf
-
