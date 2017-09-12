a4ac33c0d6933a9555597110990dd5664540df49
Art Davis (tensor) <art.davis@gmail.com>
2017-09-12 16:31:58 -0400

    Rocketry info and script update

--- build.sh
+++ build.sh
@@ -4,10 +4,13 @@
 # Build the pdf and html documentation
 #
 
-#a2x -f pdf --verbose --icons --dblatex-opts='-P latex.output.revhistory=0 --verbose --fig-path=/etc/asciidoc/images/icons' Young_Engineers_Club_Curriculum.adoc
-a2x -f pdf --icons --dblatex-opts='-P latex.output.revhistory=0 --fig-path=/etc/asciidoc/images/icons' Young_Engineers_Club_Curriculum.adoc
-a2x -f xhtml --icons --dblatex-opts='-P latex.output.revhistory=0' Young_Engineers_Club_Curriculum.adoc
+#INFILE="Young_Engineers_Club_Curriculum.adoc"
+INFILE="test.adoc"
+
+#a2x -f pdf --verbose --icons --dblatex-opts='-P latex.output.revhistory=0 --verbose --fig-path=/etc/asciidoc/images/icons' $INFILE
+a2x -f pdf --icons --dblatex-opts='-P latex.output.revhistory=0 --fig-path=/etc/asciidoc/images/icons' $INFILE
+a2x -f xhtml --icons --dblatex-opts='-P latex.output.revhistory=0' INFILE
 
 # If dblatex is misbehaving, use below to make pdf from html
-#asciidoc -b xhtml11 -d book Young_Engineers_Club_Curriculum.adoc
+#asciidoc -b xhtml11 -d book $INFILE
 #wkhtmltopdf Young_Engineers_Club_Curriculum.html Young_Engineers_Club_Curriculum.pdf
