5002b71b6fdfecac6e74176831aa34ba282a97d3
Art Davis (tensor) <art.davis@gmail.com>
2017-10-10 18:04:08 -0400

    Add 3D Printing fidget spinner workshop

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@
 # Build the pdf and html documentation
 #
 
-#INFILE="Young_Engineers_Club_Curriculum.adoc"
-INFILE="test.adoc"
+INFILE="Young_Engineers_Club_Curriculum.adoc"
+#INFILE="test.adoc"
 
-#a2x -f pdf --verbose --icons --dblatex-opts='-P latex.output.revhistory=0 --verbose --fig-path=/etc/asciidoc/images/icons' $INFILE
-a2x -f pdf --icons --dblatex-opts='-P latex.output.revhistory=0 --fig-path=/etc/asciidoc/images/icons' $INFILE
-a2x -f xhtml --icons --dblatex-opts='-P latex.output.revhistory=0' INFILE
+a2x -f pdf --verbose --icons --dblatex-opts='-P latex.output.revhistory=0 --verbose --fig-path=/etc/asciidoc/images/icons' $INFILE
+#a2x -f pdf --icons --dblatex-opts='-P latex.output.revhistory=0 --fig-path=/etc/asciidoc/images/icons' $INFILE
+#a2x -f xhtml --icons --dblatex-opts='-P latex.output.revhistory=0' INFILE
 
 # If dblatex is misbehaving, use below to make pdf from html
 #asciidoc -b xhtml11 -d book $INFILE
